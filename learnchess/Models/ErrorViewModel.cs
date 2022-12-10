93f49f28b9f1c6ea238210c9bcb762cbef16c7d3
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2022-12-10 15:37:24 +0100

    Add project files.

---
+++ learnchess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace learnchess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
