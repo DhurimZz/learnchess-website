75f900a33b698da49ed6101c4b94641b1c5a22a8
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2023-01-19 17:57:11 +0100

    Role
    seed role into database, create roleController, Role view...

---
+++ learnchess/Controllers/RolesController.cs
@@ -0,0 +1,21 @@
+﻿using learnchess.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace learnchess.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RolesController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Roles.ToListAsync());
+        }
+    }
+}
