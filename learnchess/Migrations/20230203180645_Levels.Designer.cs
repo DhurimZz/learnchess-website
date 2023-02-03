5d817ee03fc3c425ccba265fccfb439fe3ba2928
Arbnor Lama <al55544@hotmail.com>
2023-02-03 21:25:08 +0100

    CRUD Level

--- learnchess/Migrations/20230203180645_Levels.Designer.cs
+++ learnchess/Migrations/20230203180645_Levels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using learnchess.Areas.Identity.Data;
 
@@ -11,9 +12,10 @@ using learnchess.Areas.Identity.Data;
 namespace learnchess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230203180645_Levels")]
+    partial class Levels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ learnchess/Models/Levels.cs
@@ -0,0 +1,8 @@
+﻿namespace learnchess.Models
+{
+    public class Levels
+    {
+        public int Id { get; set; }
+        public string Level { get; set; }
+    }
+}
