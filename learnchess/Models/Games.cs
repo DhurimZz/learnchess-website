0feded287487efb4b4bac2920ae04a91263ba2d9
Fisnik Hyseni <fh51748@ubt-uni.net>
2023-02-08 13:34:23 +0100

    GamesModel

---
+++ learnchess/Models/Games.cs
@@ -0,0 +1,10 @@
+﻿namespace learnchess.Models
+{
+	public class Games
+	{
+		public int GamesId { get; set; }
+		public byte[]? Thumbnail { get; set; }
+		public string Title { get; set; }
+		public string Url { get; set; }
+	}
+}
